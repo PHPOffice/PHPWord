6f2297ea9ad6f608c2f1bd2dbfe82ffe4999d8fc
Gabriel Bull <me@gabrielbull.com>
2014-03-02 13:11:33 -0500

    Added PSR-4 Autoloader

--- Tests/PHPWord/AutoloaderTest.php
+++ Tests/PHPWord/AutoloaderTest.php
@@ -3,6 +3,7 @@ namespace PHPWord\Tests;
 
 use PHPUnit_Framework_TestCase;
 use PHPWord_Autoloader;
+use PHPWord_Autoloader as Autoloader;
 
 class AutoloaderTest extends PHPUnit_Framework_TestCase
 {
@@ -10,11 +11,22 @@ class AutoloaderTest extends PHPUnit_Framework_TestCase
     {
         PHPWord_Autoloader::register();
         $this->assertContains(array('PHPWord_Autoloader', 'load'), spl_autoload_functions());
+        $this->assertContains(array('PHPWord_Autoloader', 'autoload'), spl_autoload_functions());
+    }
+
+    public function testAutoloadLegacy()
+    {
+        $this->assertNull(PHPWord_Autoloader::load('Foo'), 'PHPWord_Autoloader::load() is trying to load classes outside of the PHPWord namespace');
+        $this->assertTrue(PHPWord_Autoloader::load('PHPWord'), 'PHPWord_Autoloader::load() failed to autoload the PHPWord class');
     }
 
     public function testAutoload()
     {
-        $this->assertNull(PHPWord_Autoloader::load('Foo'), 'PHPWord_Autoloader::load() is trying to load classes outside of the PHPWord namespace');
-        $this->assertTrue(PHPWord_Autoloader::load('PHPWord'), 'PHPWord_Autoloader::load() failed to autoload the PHPWord class');
+        $declared = get_declared_classes();
+        $declaredCount = count($declared);
+        Autoloader::autoload('Foo');
+        $this->assertEquals($declaredCount, count(get_declared_classes()), 'PHPWord\\Autoloader::autoload() is trying to load classes outside of the PHPWord namespace');
+        Autoloader::autoload('PHPWord\\Exceptions\\InvalidStyleException'); // TODO change this class to the main PHPWord class when it is namespaced
+        $this->assertTrue(in_array('PHPWord\\Exceptions\\InvalidStyleException', get_declared_classes()), 'PHPWord\\Autoloader::autoload() failed to autoload the PHPWord\\Exceptions\\InvalidStyleException class');
     }
 }
