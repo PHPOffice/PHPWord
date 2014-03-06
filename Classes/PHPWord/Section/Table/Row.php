f1c2c2fe5ec87956bda5ca83767ffddeeeeab48d
Ivan Lanin <ivanlanin@gmail.com>
2014-03-06 22:06:01 +0700

    Copyright year

--- Classes/PHPWord/Section/Table/Row.php
+++ Classes/PHPWord/Section/Table/Row.php
@@ -2,7 +2,7 @@
 /**
  * PHPWord
  *
- * Copyright (c) 2013 PHPWord
+ * Copyright (c) 2014 PHPWord
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
