a78ba1c925bdfd390dffdb93d0e4b2f015b60cb8
Ivan Lanin <ivanlanin@gmail.com>
2014-03-12 21:57:59 +0700

    Remove duplicate on SectionTest

--- Tests/PHPWord/SectionTest.php
+++ Tests/PHPWord/SectionTest.php
@@ -44,10 +44,4 @@ class SectionTest extends \PHPUnit_Framework_TestCase
         $oSection = new PHPWord_Section(0);
         $this->assertAttributeEquals($oSection->getHeaders(), '_headers', new PHPWord_Section(0));
     }
-
-    public function testGetElements()
-    {
-        $oSection = new PHPWord_Section(0);
-        $this->assertAttributeEquals($oSection->getElements(), '_elementCollection', new PHPWord_Section(0));
-    }
 }
