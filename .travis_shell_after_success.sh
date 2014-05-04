a62a58afc3b6d0230ddab40679f09107d4c2d106
Progi1984 <progi1984@gmail.com>
2014-05-04 12:21:19 +0200

    #154 : PHPDocumentor on GH-Pages

---
+++ .travis_shell_after_success.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "--DEBUG--"
+echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+echo "TRAVIS_PHP_VERSION: $TRAVIS_PHP_VERSION"
+echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+
+if [ "$TRAVIS_REPO_SLUG" == "PHPOffice/PHPWord" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "5.5" ]; then
+
+  echo -e "Publishing PHPDoc...\n"
+
+  cp -R build/docs $HOME/docs-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Progi1984/YATSPHP gh-pages > /dev/null
+
+  cd gh-pages
+  echo "--DEBUG : Suppression"
+  git rm -rf ./docs/$TRAVIS_BRANCH
+
+  echo "--DEBUG : Dossier"
+  mkdir docs
+  cd docs
+  mkdir $TRAVIS_BRANCH
+
+  echo "--DEBUG : Copie"
+  cp -Rf $HOME/docs-latest/* ./$TRAVIS_BRANCH/
+
+  echo "--DEBUG : Git"
+  git add -f .
+  git commit -m "PHPDocumentor (Travis Build : $TRAVIS_BUILD_NUMBER  - Branch : $TRAVIS_BRANCH)"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published PHPDoc to gh-pages.\n"
+
+fi
