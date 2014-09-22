0a8635ece80dbb6f2dcf94f808f0fa8769be4546
Spike Ilacqua <spike@indra.com>
2014-09-22 16:58:00 -0600

    Add missing setters for pageSizeW and pageSizeH

--- src/PhpWord/Style/Section.php
+++ src/PhpWord/Style/Section.php
@@ -239,6 +239,13 @@ class Section extends Border
         return $this->pageSizeW;
     }
 
+    public function setPageSizeW($value = null)
+    {
+        $this->pageSizeW = $this->setNumericVal($value, self::DEFAULT_WIDTH);
+
+        return $this;
+    }
+
     /**
      * Get Page Size Height
      *
@@ -247,6 +254,13 @@ class Section extends Border
     public function getPageSizeH()
     {
         return $this->pageSizeH;
+    }
+
+    public function setPageSizeH($value = null)
+    {
+        $this->pageSizeH = $this->setNumericVal($value, self::DEFAULT_HEIGHT);
+
+        return $this;
     }
 
     /**
