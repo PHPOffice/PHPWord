caefa208026a90c2b936e10a0deb15884b7306a3
Alexis Quiroz <kirochi@hotmail.com>
2014-11-20 11:27:58 -0300

    Update Template.php

--- src/PhpWord/Template.php
+++ src/PhpWord/Template.php
@@ -223,7 +223,34 @@ class Template
 
         $this->documentXML = $result;
     }
+    
+ /*
+     * write multi variable in mutiBlock
+     * @param string $blockname
+     * @param array $arraySet format array('nameOne' => 'Alexis', 'nameTwo' => 'Quiroz')
+     * @return string|null 
+     */
 
+    public function writeMultiBlock($blockname, $arraySet = '') {
+        $xmlBlock = null;
+        preg_match('/(<\?xml.*)(<w:p.*>\${' . $blockname . '}<\/w:.*?p>)(.*)(<w:p.*\${\/' . $blockname . '}<\/w:.*?p>)/is', $this->documentXML, $matches);
+        if (isset($matches[3])) {          
+            $xmlBlock = $matches[3];
+            $blockComplet = $matches[2] . $matches[3] . $matches[4];
+
+            foreach($arraySet as $nameVar => $value){
+                $this->setValue($nameVar, $value);
+            }
+            
+
+        
+            $this->documentXML = str_replace($matches[2], '', $this->documentXML);
+            $this->documentXML = str_replace($matches[4], $blockComplet, $this->documentXML);
+        }
+
+        return $xmlBlock;
+    }
+    
     /**
      * Clone a block
      *
