25342870be9681054446fddf5ccc27a8f7e1045d
Raphael C. Silva <raphaelcsilva@users.noreply.github.com>
2015-08-25 21:08:40 -0300

    Refactoring in CLI constant  definition

--- samples/Sample_Header.php
+++ samples/Sample_Header.php
@@ -10,7 +10,7 @@ use PhpOffice\PhpWord\Autoloader;
 use PhpOffice\PhpWord\Settings;
 
 error_reporting(E_ALL);
-define('CLI', (PHP_SAPI == 'cli') ? true : false);
+define('CLI', PHP_SAPI == 'cli');
 define('EOL', CLI ? PHP_EOL : '<br />');
 define('SCRIPT_FILENAME', basename($_SERVER['SCRIPT_FILENAME'], '.php'));
 define('IS_INDEX', SCRIPT_FILENAME == 'index');
