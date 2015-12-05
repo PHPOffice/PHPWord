43fb23d89691f373678555fbb0895b4fec234f26
Roman Syroeshko <syroeshkoro@gmail.com>
2015-12-05 21:25:59 +0400

    Copyright update.

--- src/PhpWord/Collection/AbstractCollection.php
+++ src/PhpWord/Collection/AbstractCollection.php
@@ -11,8 +11,8 @@
  * contributors, visit https://github.com/PHPOffice/PHPWord/contributors.
  *
  * @link        https://github.com/PHPOffice/PHPWord
- * @copyright   2010-2014 PHPWord contributors
- * @license     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt LGPL
+ * @copyright   2010-2015 PHPWord contributors
+ * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  */
 
 namespace PhpOffice\PhpWord\Collection;
