7bd6cbdb64a40241e1240be36752bd1c1b25c006
Roman Syroeshko <syroeshkoro@gmail.com>
2015-12-05 21:28:46 +0400

    Copyright update.

--- src/PhpWord/PhpWord.php
+++ src/PhpWord/PhpWord.php
@@ -13,7 +13,7 @@
  * @link        https://github.com/PHPOffice/PHPWord
  * @copyright   2010-2015 PHPWord contributors
  * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
- */
+*/
 
 namespace PhpOffice\PhpWord;
 
