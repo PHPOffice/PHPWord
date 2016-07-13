87c20b232364bf5931a5852e2d1442f9b21c17c3
Maxim Barulin <mbarulin@gmail.com>
2016-07-13 13:50:56 +0300

    tbody and cell width

--- src/PhpWord/Shared/Html.php
+++ src/PhpWord/Shared/Html.php
@@ -123,6 +123,7 @@ class Html
             'sup'       => array('Property',    null,   null,       $styles,    null,   'superScript',  true),
             'sub'       => array('Property',    null,   null,       $styles,    null,   'subScript',    true),
             'table'     => array('Table',       $node,  $element,   $styles,    null,   'addTable',     true),
+            'tbody'     => array('Table',       $node,  $element,   $styles,    null,   'skipTbody',    true), //added to catch tbody in html.
             'tr'        => array('Table',       $node,  $element,   $styles,    null,   'addRow',       true),
             'td'        => array('Table',       $node,  $element,   $styles,    null,   'addCell',      true),
             'ul'        => array('List',        null,   null,       $styles,    $data,  3,              null),
@@ -176,7 +177,14 @@ class Html
             $cNodes = $node->childNodes;
             if (count($cNodes) > 0) {
                 foreach ($cNodes as $cNode) {
-                    if ($element instanceof AbstractContainer) {
+                    // Added to get tables to work
+                    $htmlContainers = array(
+                        'tbody',
+                        'tr',
+                        'td',
+                    );
+                    if (in_array( $cNode->nodeName, $htmlContainers )
+                        || $element instanceof AbstractContainer ) {
                         self::parseNode($cNode, $element, $styles, $data);
                     }
                 }
@@ -269,19 +277,37 @@ class Html
     private static function parseTable($node, $element, &$styles, $argument1)
     {
         $styles['paragraph'] = self::parseInlineStyle($node, $styles['paragraph']);
-
-        $newElement = $element->$argument1();
+        switch ($argument1) {
+            case 'addTable':
+                $styles['paragraph'] = self::parseInlineStyle($node, $styles['paragraph']);
+                $newElement = $element->addTable('table', array('width' => 90));
+                break;
+            case 'skipTbody':
+                $newElement = $element;
+                break;
+            case 'addRow':
+                $newElement = $element->addRow();
+                break;
+            case 'addCell':
+                $width = null;
+                if (array_key_exists('width', $styles['paragraph'])) {
+                    $width = (int)$styles['paragraph']['width'] * 15; //in twip
+                    unset($styles['paragraph']['width']);
+                }
+                $newElement = $element->addCell($width);
+                break;
+        }
 
         // $attributes = $node->attributes;
         // if ($attributes->getNamedItem('width') !== null) {
-            // $newElement->setWidth($attributes->getNamedItem('width')->value);
+        // $newElement->setWidth($attributes->getNamedItem('width')->value);
         // }
 
         // if ($attributes->getNamedItem('height') !== null) {
-            // $newElement->setHeight($attributes->getNamedItem('height')->value);
+        // $newElement->setHeight($attributes->getNamedItem('height')->value);
         // }
         // if ($attributes->getNamedItem('width') !== null) {
-            // $newElement=$element->addCell($width=$attributes->getNamedItem('width')->value);
+        // $newElement=$element->addCell($width=$attributes->getNamedItem('width')->value);
         // }
 
         return $newElement;
@@ -392,6 +418,9 @@ class Html
                     break;
                 case 'background-color':
                     $styles['bgColor'] = trim($cValue, "#");
+                    break;
+                case 'width':
+                    $styles['width'] = substr( $cValue, 0, -2);;
                     break;
             }
         }
