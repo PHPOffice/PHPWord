50cee189c6cb71943338a5ed78cc5e83dcc04a56
Maxim Barulin <mbarulin@gmail.com>
2016-07-13 13:06:09 +0300

    fix

--- src/PhpWord/Shared/Html.php
+++ src/PhpWord/Shared/Html.php
@@ -377,7 +377,7 @@ class Html
                     break;
                 case 'text-align':
 					//ckeditor insert justify instead of both
-					if(strtolower($cValue) === 'justify') $cValue = 'both'
+					if(strtolower($cValue) === 'justify') $cValue = 'both';
                     $styles['alignment'] = $cValue; // todo: any mapping?
                     break;
                 case 'font-size':
