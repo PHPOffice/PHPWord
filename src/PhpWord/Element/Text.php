e45f570e88a52523189b9845f144ea5978809807
Mihail Krastev <mlinuxgada@gmail.com>
2016-07-19 18:31:43 +0300

    Simple fix for being able to use the String/renamed to StringComponent/ in php7

--- src/PhpWord/Element/Text.php
+++ src/PhpWord/Element/Text.php
@@ -17,7 +17,7 @@
 
 namespace PhpOffice\PhpWord\Element;
 
-use PhpOffice\PhpWord\Shared\String;
+use PhpOffice\PhpWord\Shared\StringComponent;
 use PhpOffice\PhpWord\Style\Font;
 use PhpOffice\PhpWord\Style\Paragraph;
 
@@ -136,7 +136,7 @@ class Text extends AbstractElement
      */
     public function setText($text)
     {
-        $this->text = String::toUTF8($text);
+        $this->text = StringComponent::toUTF8($text);
 
         return $this;
     }
