2bcfacfbf9ee8553ea06750ec311a4a45ead4beb
Roman Syroeshko <syroeshkoro@gmail.com>
2016-07-31 12:35:06 +0400

    Updated copyright.

--- src/PhpWord/Element/AbstractElement.php
+++ src/PhpWord/Element/AbstractElement.php
@@ -11,7 +11,7 @@
  * contributors, visit https://github.com/PHPOffice/PHPWord/contributors.
  *
  * @link        https://github.com/PHPOffice/PHPWord
- * @copyright   2010-2015 PHPWord contributors
+ * @copyright   2010-2016 PHPWord contributors
  * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  */
 
@@ -19,7 +19,6 @@ namespace PhpOffice\PhpWord\Element;
 
 use PhpOffice\PhpWord\Media;
 use PhpOffice\PhpWord\PhpWord;
-use PhpOffice\PhpWord\Style;
 
 /**
  * Element abstract class
