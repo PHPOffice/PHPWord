eff532e64fe4223dd8dbcb1d4bf2a70031749f4d
antoine <antoine.de.troostembergh@gmail.com>
2017-06-17 01:01:54 +0200

    make code php 5.3 -> 5.5 compatible

--- tests/PhpWord/SimpleType/FootnotePropertiesTest.php
+++ tests/PhpWord/SimpleType/FootnotePropertiesTest.php
@@ -15,7 +15,7 @@
  * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  */
 
-namespace PhpOffice\PhpWord\Style;
+namespace PhpOffice\PhpWord\SimpleType;
 
 use PhpOffice\PhpWord\SimpleType\FootnoteProperties;
 
