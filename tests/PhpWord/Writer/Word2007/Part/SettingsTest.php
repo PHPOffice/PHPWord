5d5362a3fda20d3e79c089510a19e696d836cf63
troosan <antoine.de.troostembergh@gmail.com>
2017-12-14 00:15:23 +0100

    sort imports

--- tests/PhpWord/Writer/Word2007/Part/SettingsTest.php
+++ tests/PhpWord/Writer/Word2007/Part/SettingsTest.php
@@ -21,10 +21,10 @@ use PhpOffice\PhpWord\ComplexType\ProofState;
 use PhpOffice\PhpWord\ComplexType\TrackChangesView;
 use PhpOffice\PhpWord\PhpWord;
 use PhpOffice\PhpWord\Settings;
+use PhpOffice\PhpWord\Shared\Microsoft\PasswordEncoder;
 use PhpOffice\PhpWord\SimpleType\Zoom;
 use PhpOffice\PhpWord\Style\Language;
 use PhpOffice\PhpWord\TestHelperDOCX;
-use PhpOffice\PhpWord\Shared\Microsoft\PasswordEncoder;
 
 /**
  * Test class for PhpOffice\PhpWord\Writer\Word2007\Part\Settings
