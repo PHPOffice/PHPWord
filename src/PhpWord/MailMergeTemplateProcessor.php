9a85ec0c319ac97b3b344bb36125a72f1f905c90
korvinko <john.doe.jtg@gmail.com>
2018-11-19 19:54:53 +0300

    Added set values

--- src/PhpWord/MailMergeTemplateProcessor.php
+++ src/PhpWord/MailMergeTemplateProcessor.php
@@ -72,6 +72,24 @@ class MailMergeTemplateProcessor extends TemplateProcessor
         }
     }
 
+    public function setValues()
+    {
+        $this->mergeData = array_change_key_case($this->mergeData, CASE_UPPER);
+
+        foreach ($this->tempDocumentHeaders as $index => $headerXML) {
+            $this->temporarySectionName = 'header' . $index;
+            $this->tempDocumentHeaders[$index] = $this->setValuesForPart($this->tempDocumentHeaders[$index]);
+        }
+
+        $this->temporarySectionName = 'document';
+        $this->tempDocumentMainPart = $this->setValuesForPart($this->tempDocumentMainPart);
+
+        foreach ($this->tempDocumentFooters as $index => $headerXML) {
+            $this->temporarySectionName = 'footer' . $index;
+            $this->tempDocumentFooters[$index] = $this->setValuesForPart($this->tempDocumentFooters[$index]);
+        }
+    }
+
     /**
      * @return string[]
      */
@@ -132,7 +150,65 @@ class MailMergeTemplateProcessor extends TemplateProcessor
             $this->mergeFailure[$sec] = (isset($this->mergeFailure[$sec]) ? $this->mergeFailure[$sec] : 0) + 1;
         }
 
-        $final = preg_replace('/(<w:t.*?>)(.+?)(<\/w:t>)/si', '${1}' . $newval . '${3}', $final);
+        $final = preg_replace('/(<w:t.*?>)(.+?)(<\/w:t>)/si', "\${1}$newval\${3}", $final);
+
+        return $final;
+    }
+
+
+    /**
+     * Find and replace merge fields in the given XML section.
+     * @return string
+     */
+    protected function setValuesForPart($documentPartXML)
+    {
+        // break down major sections <w:p>
+        return preg_replace_callback("/<w:p[\s>].+?<\/w:p>/si", array($this, 'setValuesForSection'),
+            $documentPartXML);
+    }
+
+    /**
+     * Find and replace merge fields in the given XML section.
+     * @return string[]
+     */
+    protected function setValuesForSection($replace)
+    {
+        $section = $replace[0];
+        $section = preg_replace('/<\/w:instrText><\/w:r><w:r\s+w:rsidR="\w+"><w:instrText\s+xml:space="preserve">/uis',
+            '', $section);
+
+        return preg_replace_callback(
+            "/(<w:r[\s>]((?!<\/w:r>).)*?<w:fldChar\s+w:fldCharType=\"begin\"\/>.*?<\/w:r>)\s*(<w:r[\s>].+?\s+MERGEFIELD\s+\"*\w+\"*\s+.+?<\/w:r>)\s*(<w:r[\s>].*?<w:fldChar\s+w:fldCharType=\"separate\"\/>.*?<\/w:r>)\s*(<w:r[\s>].+?<\/w:r>)\s*(<w:r[\s>].*?<w:fldChar\s+w:fldCharType=\"end\"\/>.*?<\/w:r>)/uis",
+            array($this, 'setValuesForMergeField'), $section);
+    }
+
+    protected function setValuesForMergeField($replace)
+    {
+        /* $replace: array 1..x corresponds to () matches in preg_replace */
+        $field = $replace[3];
+        $final = $replace[5];
+
+        if (preg_match('/(?:<w:instrText.+?\s+MERGEFIELD\s+\"*)(\w+)(?:\"*\s+<\/w:instrText>)/sui', $field, $match)) {
+            $key = strtoupper($match[1]);
+        } else {
+            return $replace[0];
+        }
+
+        $sec = $this->temporarySectionName . '/' . $key;
+
+        if (isset($this->mergeData[$key])) {
+            /* success */
+            $newval = $this->mergeData[$key];
+            $this->mergeSuccess[$sec] = (isset($this->mergeSuccess[$sec]) ? $this->mergeSuccess[$sec] : 0) + 1;
+        } else {
+            /* failure */
+            $newval = $key;
+            $this->mergeFailure[$sec] = (isset($this->mergeFailure[$sec]) ? $this->mergeFailure[$sec] : 0) + 1;
+        }
+
+        $final = preg_replace('/(<w:t.*?>)(.+?)(<\/w:t>)/si', "\${1}$newval\${3}", $final);
+        $final = "$replace[1]$replace[3]$replace[4]$final$replace[6]";
+
         return $final;
     }
 }
