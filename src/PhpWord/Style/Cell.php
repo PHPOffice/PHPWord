04b224caa5b5dca5b75fdf6f84649f0786812eee
rikvdlooi <rik@duodeka.nl>
2020-04-15 22:34:19 +0200

    Fixed setting width of Cell Style
    The setWidth method did not change the width in the style, seems to be a small mistake.
    Now the width is correctly for the Cell Style.

--- src/PhpWord/Style/Cell.php
+++ src/PhpWord/Style/Cell.php
@@ -299,7 +299,7 @@ class Cell extends Border
      */
     public function setWidth($value)
     {
-        $this->setIntVal($value);
+        $this->width = $this->setIntVal($value);
 
         return $this;
     }
