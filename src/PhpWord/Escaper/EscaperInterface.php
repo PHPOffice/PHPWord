38c9ff1e406e755bbe9a3fc5851065f935b620a3
wandrewchan <wandrewcworkshop@gmail.com>
2020-08-14 13:53:02 +0800

    Update EscaperInterface.php

--- src/PhpWord/Escaper/EscaperInterface.php
+++ src/PhpWord/Escaper/EscaperInterface.php
@@ -30,4 +30,39 @@ interface EscaperInterface
      * @return mixed
      */
     public function escape($input);
+    
+    /**
+     * @param mixed $input
+     *
+     * @return mixed
+     */
+    public function escapeHtml($input);
+    
+    /**
+     * @param mixed $input
+     *
+     * @return mixed
+     */
+    public function escapeJs($input);
+    
+    /**
+     * @param mixed $input
+     *
+     * @return mixed
+     */
+    public function escapeCss($input);
+    
+    /**
+     * @param mixed $input
+     *
+     * @return mixed
+     */
+    public function escapeHtmlAttr($input);
+    
+    /**
+     * @param mixed $input
+     *
+     * @return mixed
+     */
+    public function escapeUrl($input);
 }
