e5f4feacd0ca3bf6e82e2ad1c94e06d107d10e1a
Tony <git config --global user.email you@example.com>
2021-09-20 21:44:20 +0300

    BA-3673 fix dynamic year

--- src/PhpWord/Writer/Word2007/Part/Chart.php
+++ src/PhpWord/Writer/Word2007/Part/Chart.php
@@ -651,10 +651,9 @@ class Chart extends AbstractPart
             }
 
             if ($this->element->getType() == 'scatter') {
-                if ((count($categories) <= 12 && !in_array($style->getFormat(), ['month', 'week']))
+                if ((count($categories) <= 12 && !in_array($style->getFormat(), ['month', 'week', 'year']))
                     || ($style->getFormat() == 'day' && $categories[array_key_last($categories)] - $categories[array_key_first($categories)] <= 10)
                     || ($style->getFormat() == 'week' && $categories[array_key_last($categories)] - $categories[array_key_first($categories)] < 7)
-                    || ($style->getFormat() == 'year')
                 ) {
                     $xmlWriter->writeElementBlock('c:majorUnit', 'val', 1);
                 }
