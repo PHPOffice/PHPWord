25575c80ca1ec1a496eba09882a9cada56626190
Erik Hazington <83652897+hazington@users.noreply.github.com>
2022-12-30 21:34:47 +0100

    Add background color support for text boxes for Word writer

--- src/PhpWord/Writer/Word2007/Element/TextBox.php
+++ src/PhpWord/Writer/Word2007/Element/TextBox.php
@@ -50,6 +50,9 @@ class TextBox extends Image
         $xmlWriter->startElement('v:shape');
         $xmlWriter->writeAttribute('type', '#_x0000_t0202');
 
+        if ($style->getBgColor()) {
+            $xmlWriter->writeAttribute('fillcolor', $style->getBgColor());
+        }
         $styleWriter->write();
         $styleWriter->writeBorder();
 
