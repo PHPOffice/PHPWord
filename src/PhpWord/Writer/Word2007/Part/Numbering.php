ecb9e6bfdd8c4cc44e1cb5f324d877970288c624
DK <dawid.krecisz@mailmetrics.com>
2022-12-30 18:54:04 +0100

    Numbering style

--- src/PhpWord/Writer/Word2007/Part/Numbering.php
+++ src/PhpWord/Writer/Word2007/Part/Numbering.php
@@ -19,8 +19,10 @@ namespace PhpOffice\PhpWord\Writer\Word2007\Part;
 
 use PhpOffice\PhpWord\Shared\XMLWriter;
 use PhpOffice\PhpWord\Style;
+use PhpOffice\PhpWord\Style\Font;
 use PhpOffice\PhpWord\Style\Numbering as NumberingStyle;
 use PhpOffice\PhpWord\Style\NumberingLevel;
+use PhpOffice\PhpWord\Writer\Word2007\Style\Shading;
 
 /**
  * Word2007 numbering part writer: word/numbering.xml.
@@ -172,12 +174,68 @@ class Numbering extends AbstractPart
         $hint = $level->getHint();
 
         $xmlWriter->startElement('w:rPr');
+
+        $style = Style::getStyle('List '. $level->getLevel());
+        if ($style instanceof Font) {
+            // Color
+            $color = $style->getColor();
+            $xmlWriter->writeElementIf($color !== null, 'w:color', 'w:val', $color);
+
+            // Size
+            $size = $style->getSize();
+            $xmlWriter->writeElementIf($size !== null, 'w:sz', 'w:val', $size * 2);
+            $xmlWriter->writeElementIf($size !== null, 'w:szCs', 'w:val', $size * 2);
+
+            // Bold, italic
+            $xmlWriter->writeElementIf($style->isBold() !== null, 'w:b', 'w:val', $this->writeOnOf($style->isBold()));
+            $xmlWriter->writeElementIf($style->isBold() !== null, 'w:bCs', 'w:val', $this->writeOnOf($style->isBold()));
+            $xmlWriter->writeElementIf($style->isItalic() !== null, 'w:i', 'w:val', $this->writeOnOf($style->isItalic()));
+            $xmlWriter->writeElementIf($style->isItalic() !== null, 'w:iCs', 'w:val', $this->writeOnOf($style->isItalic()));
+
+            // Strikethrough, double strikethrough
+            $xmlWriter->writeElementIf($style->isStrikethrough() !== null, 'w:strike', 'w:val', $this->writeOnOf($style->isStrikethrough()));
+            $xmlWriter->writeElementIf($style->isDoubleStrikethrough() !== null, 'w:dstrike', 'w:val', $this->writeOnOf($style->isDoubleStrikethrough()));
+
+            // Small caps, all caps
+            $xmlWriter->writeElementIf($style->isSmallCaps() !== null, 'w:smallCaps', 'w:val', $this->writeOnOf($style->isSmallCaps()));
+            $xmlWriter->writeElementIf($style->isAllCaps() !== null, 'w:caps', 'w:val', $this->writeOnOf($style->isAllCaps()));
+
+            //Hidden text
+            $xmlWriter->writeElementIf($style->isHidden(), 'w:vanish', 'w:val', $this->writeOnOf($style->isHidden()));
+
+            // Underline
+            $xmlWriter->writeElementIf($style->getUnderline() != 'none', 'w:u', 'w:val', $style->getUnderline());
+
+            // Foreground-Color
+            $xmlWriter->writeElementIf($style->getFgColor() !== null, 'w:highlight', 'w:val', $style->getFgColor());
+
+            // Superscript/subscript
+            $xmlWriter->writeElementIf($style->isSuperScript(), 'w:vertAlign', 'w:val', 'superscript');
+            $xmlWriter->writeElementIf($style->isSubScript(), 'w:vertAlign', 'w:val', 'subscript');
+
+            // Spacing
+            $xmlWriter->writeElementIf($style->getScale() !== null, 'w:w', 'w:val', $style->getScale());
+            $xmlWriter->writeElementIf($style->getSpacing() !== null, 'w:spacing', 'w:val', $style->getSpacing());
+            $xmlWriter->writeElementIf($style->getKerning() !== null, 'w:kern', 'w:val', $style->getKerning() * 2);
+
+            // noProof
+            $xmlWriter->writeElementIf($style->isNoProof() !== null, 'w:noProof', 'w:val', $this->writeOnOf($style->isNoProof()));
+
+            // Background-Color
+            $shading = $style->getShading();
+            if (null !== $shading) {
+                $styleWriter = new Shading($xmlWriter, $shading);
+                $styleWriter->write();
+            }
+        }
+
         $xmlWriter->startElement('w:rFonts');
         $xmlWriter->writeAttributeIf($font !== null, 'w:ascii', $font);
         $xmlWriter->writeAttributeIf($font !== null, 'w:hAnsi', $font);
         $xmlWriter->writeAttributeIf($font !== null, 'w:cs', $font);
         $xmlWriter->writeAttributeIf($hint !== null, 'w:hint', $hint);
         $xmlWriter->endElement(); // w:rFonts
+
         $xmlWriter->endElement(); // w:rPr
     }
 
