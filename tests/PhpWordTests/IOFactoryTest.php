3fdd8b40d95ed1d916d8fe3b67a0156f523daad1
Marin Nikolli <sibalonat@gmail.com>
2023-11-30 13:31:18 +0100

    whiteline

--- tests/PhpWordTests/IOFactoryTest.php
+++ tests/PhpWordTests/IOFactoryTest.php
@@ -124,8 +124,8 @@ class IOFactoryTest extends TestCase
     {
         $file = __DIR__ . '/_files/templates/extract-variable.docx';
         $extractedVariables = IOFactory::extractVariables($file, 'Word2007');
-        
         $expectedVariables = ['date', 'A1', 'B1']; 
+
         self::assertEquals($expectedVariables, $extractedVariables, 'Extracted variables do not match expected variables.');
     }
 }
