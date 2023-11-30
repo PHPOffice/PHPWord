41beb2db578855d62fc5a6abb73525c51921f7da
Marin Nikolli <sibalonat@gmail.com>
2023-11-30 13:24:38 +0100

    white space

--- samples/Sample_44_ExtractVariablesFromReaderWord2007.php
+++ samples/Sample_44_ExtractVariablesFromReaderWord2007.php
@@ -4,8 +4,6 @@ include_once 'Sample_Header.php';
 
 // Read contents
 $name = basename(__FILE__, '.php');
-
 $source = __DIR__ . "/resources/{$name}.docx";
-
 echo date('H:i:s'), " Reading contents from `{$source}`", EOL;
 $phpWord = \PhpOffice\PhpWord\IOFactory::extractVariables($source);
