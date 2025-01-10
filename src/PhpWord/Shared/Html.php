03ad7ecffef7d6410bac57bc7e8c612444031e38
oleibman <10341515+oleibman@users.noreply.github.com>
2025-01-09 17:41:43 -0800

    Php-cs-fixer Recommendations

--- src/PhpWord/Shared/Html.php
+++ src/PhpWord/Shared/Html.php
@@ -303,7 +303,7 @@ class Html
      * @param AbstractContainer $element
      * @param array &$styles
      *
-     * @return \PhpOffice\PhpWord\Element\PageBreak|\PhpOffice\PhpWord\Element\TextRun
+     * @return \PhpOffice\PhpWord\Element\PageBreak|TextRun
      */
     protected static function parseParagraph($node, $element, &$styles)
     {
@@ -466,7 +466,7 @@ class Html
      * @param Table $element
      * @param array &$styles
      *
-     * @return \PhpOffice\PhpWord\Element\Cell|\PhpOffice\PhpWord\Element\TextRun $element
+     * @return \PhpOffice\PhpWord\Element\Cell|TextRun $element
      */
     protected static function parseCell($node, $element, &$styles)
     {
