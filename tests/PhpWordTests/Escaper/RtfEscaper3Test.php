a547f04b7dd2a247603d3556f0b0185d1a636275
rasamassen <rasamassen@gmail.com>
2025-09-08 20:59:46 -0500

    Update RtfEscaper3Test.php - Replace nowidctlpar with widctlpar - Widow/Orphan controls is default true.

--- tests/PhpWordTests/Escaper/RtfEscaper3Test.php
+++ tests/PhpWordTests/Escaper/RtfEscaper3Test.php
@@ -25,8 +25,8 @@ use PhpOffice\PhpWord\Settings;
  */
 class RtfEscaper3Test extends \PHPUnit\Framework\TestCase
 {
-    const HEADER = '\\pard\\nowidctlpar \ql{\\cf0\\f0 ';
-    const HEADER_RTL = '\\pard\\nowidctlpar \qr{\\rtlch\\cf0\\f0 ';
+    const HEADER = '\\pard\\widctlpar \ql{\\cf0\\f0 ';
+    const HEADER_RTL = '\\pard\\widctlpar \qr{\\rtlch\\cf0\\f0 ';
     const TRAILER = '}\\par';
 
     protected function tearDown(): void
