97ddc80673d4b9e4593c997d81c2da278479e47e
rasamassen <rasamassen@gmail.com>
2025-09-08 21:01:46 -0500

    Update ElementTest.php - Replace nowidctlpar with widctlpar - Widow/Orphan controls is default true.

--- tests/PhpWordTests/Writer/RTF/ElementTest.php
+++ tests/PhpWordTests/Writer/RTF/ElementTest.php
@@ -149,7 +149,7 @@ class ElementTest extends \PHPUnit\Framework\TestCase
         $element->addText('Hello ');
         $element->addText('there.');
         $textrun = new RTF\Element\TextRun($parentWriter, $element);
-        $expect = "\\pard\\nowidctlpar {{\\cf0\\f0 Hello }{\\cf0\\f0 there.}}\\par\n";
+        $expect = "\\pard\\widctlpar {{\\cf0\\f0 Hello }{\\cf0\\f0 there.}}\\par\n";
         self::assertEquals($expect, $this->removeCr($textrun));
     }
 
@@ -160,7 +160,7 @@ class ElementTest extends \PHPUnit\Framework\TestCase
         $element->addText('Hello ');
         $element->addText('there.');
         $textrun = new RTF\Element\TextRun($parentWriter, $element);
-        $expect = "\\pard\\nowidctlpar \\sb0\\sa0{{\\cf0\\f0 Hello }{\\cf0\\f0 there.}}\\par\n";
+        $expect = "\\pard\\widctlpar \\sb0\\sa0{{\\cf0\\f0 Hello }{\\cf0\\f0 there.}}\\par\n";
         self::assertEquals($expect, $this->removeCr($textrun));
     }
 
@@ -172,7 +172,7 @@ class ElementTest extends \PHPUnit\Framework\TestCase
         $section = $phpWord->addSection();
         $element = $section->addTitle('First Heading', 1);
         $elwrite = new RTF\Element\Title($parentWriter, $element);
-        $expect = "\\pard\\nowidctlpar \\sb0\\sa0{\\outlinelevel0{\\cf0\\f0 First Heading}\\par\n}";
+        $expect = "\\pard\\widctlpar \\sb0\\sa0{\\outlinelevel0{\\cf0\\f0 First Heading}\\par\n}";
         self::assertEquals($expect, $this->removeCr($elwrite));
     }
 
@@ -188,7 +188,7 @@ class ElementTest extends \PHPUnit\Framework\TestCase
         $element->addRuby($baseTextRun, $rubyTextRun, $properties);
 
         $textrun = new RTF\Element\TextRun($parentWriter, $element);
-        $expect = "\\pard\\nowidctlpar {{base text (ruby)}}\\par\n";
+        $expect = "\\pard\\widctlpar {{base text (ruby)}}\\par\n";
         self::assertEquals($expect, $this->removeCr($textrun));
     }
 
@@ -213,7 +213,7 @@ class ElementTest extends \PHPUnit\Framework\TestCase
         $element = $section->addTitle($textRun, 1);
         $elwrite = new RTF\Element\Title($parentWriter, $element);
 
-        $expect = "\\pard\\nowidctlpar \\sb0\\sa2{\\outlinelevel0{\\cf0\\f0\\fs48\\b base text (ruby)}\\par\n}";
+        $expect = "\\pard\\widctlpar \\sb0\\sa2{\\outlinelevel0{\\cf0\\f0\\fs48\\b base text (ruby)}\\par\n}";
         self::assertEquals($expect, $this->removeCr($elwrite));
     }
 }
