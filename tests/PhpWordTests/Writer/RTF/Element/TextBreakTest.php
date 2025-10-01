2b590d72b77f7975ca594cd22ff9d981a3472d4a
rasamassen <rasamassen@gmail.com>
2025-10-01 15:29:28 -0500

    Create TextBreakTest.php

---
+++ tests/PhpWordTests/Writer/RTF/Element/TextBreakTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of PHPWord - A pure PHP library for reading and writing
+ * word processing documents.
+ *
+ * PHPWord is free software distributed under the terms of the GNU Lesser
+ * General Public License version 3 as published by the Free Software Foundation.
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code. For the full list of
+ * contributors, visit https://github.com/PHPOffice/PHPWord/contributors.
+ *
+ * @see         https://github.com/PHPOffice/PHPWord
+ *
+ * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
+ */
+
+namespace PhpOffice\PhpWordTests\Writer\RTF\Element;
+
+use PhpOffice\PhpWord\Element\TextBreak as TextBreakElement;
+use PhpOffice\PhpWord\Settings;
+use PhpOffice\PhpWord\Writer\RTF;
+use PhpOffice\PhpWord\Writer\RTF\Element\TextBreak as TextBreakWriter;
+use PHPUnit\Framework\TestCase;
+
+class TextBreakTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        Settings::setDefaultRtl(null);
+    }
+    
+    public function removeCr($field)
+    {
+        return str_replace("\r\n", "\n", $field->write());
+    }
+    
+    public function testTextBreakParagraph(): void
+    {
+        $parentWriter = new RTF();
+        $element = new TextBreakElement();
+        $writer = new TextBreakWriter($parentWriter, $element);
+        $expect = "\\pard\\par\n";
+        self::assertEquals($expect, $this->removeCr($writer));
+    }
+
+}
