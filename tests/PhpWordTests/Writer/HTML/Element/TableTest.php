7ad4226469d6b30ae0fadacb5079e000c394c24e
rasamassen <rasamassen@gmail.com>
2025-10-20 22:23:05 -0500

    Try again

--- tests/PhpWordTests/Writer/HTML/Element/TableTest.php
+++ tests/PhpWordTests/Writer/HTML/Element/TableTest.php
@@ -232,7 +232,7 @@ class TableTest extends TestCase
         $cell3Query = $xpath->query('//table/tr[3]/td[1]');
         self::assertNotFalse($cell3Query);
         self::assertCount(1, $cell3Query);
-        self::assertObjectHasProperty('attributes', $cell3Query->item(0));
+        self::assertClassHasAttribute($cell3Query->item(0), 'attributes');
         self::assertNull($cell3Query->item(0)->attributes->getNamedItem('rowspan'));
     }
 }
