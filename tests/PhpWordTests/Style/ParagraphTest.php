bdf8d46cb0dccfcab162bdc317ab6f49986fb7d8
Aleksandr Novikov <pwsdotru@gmail.com>
2026-07-12 22:20:45 +0300

    Fixed errors by phpstan - ignore errors for php 7.2 and 7.3

--- tests/PhpWordTests/Style/ParagraphTest.php
+++ tests/PhpWordTests/Style/ParagraphTest.php
@@ -94,7 +94,7 @@ class ParagraphTest extends \PHPUnit\Framework\TestCase
             $get = $this->findGetter($key, $value, $object);
             $object->setStyleValue("$key", $value);
             if (('indent' == $key || 'hanging' == $key)) {
-                $value = $value * 720;
+                $value = (int)$value * 720;
             }
             self::assertEquals($value, $object->$get());
         }
