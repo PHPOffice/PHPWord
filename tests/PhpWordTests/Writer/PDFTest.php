3fe5f8b8b285cf3fd28a8210abc37a9bed059b17
oleibman <10341515+oleibman@users.noreply.github.com>
2026-07-20 15:37:39 -0700

    Cleanup Phpstan After Merge

--- tests/PhpWordTests/Writer/PDFTest.php
+++ tests/PhpWordTests/Writer/PDFTest.php
@@ -91,11 +91,6 @@ class PDFTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\PhpOffice\PhpWord\Exception\Exception::class);
         $this->expectExceptionMessage('PDF rendering library or library path has not been defined.');
         $writer = new PDF(new PhpWord());
-        $writer->save($file);
-
-        //If no exception and file created
-        if (file_exists($file)) {
-            unlink($file);
-        }
+        $writer->save('php://output');
     }
 }
